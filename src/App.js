a394c34b0bfd92c5cdb86b731908a9fdcf45d1e5
Ana Carolina K <anacko@gmail.com>
2021-12-06 18:22:23 -0500

    add colors

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Challenger from './components/Challenger';
 
 function App() {
 
-  const [stage, setStage] = useState(1);
+  const [stage, setStage] = useState(0);
 
   return (
     <div className="App">
       <h1>Hello World!</h1>
       <p>Stage: {stage}</p>
+
+      <button onClick={() => setStage(stage + 1)}>Click Me!</button>
 
       <Challenger stage={stage}/>
     </div>
--- src/components/Challenger.jsx
+++ src/components/Challenger.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 import { makeSequence } from "../helpers/makeSequence";
 
+import './Challenger.css'
+
 const Challenger = function(props) {
 
-  const sequence = makeSequence(4)
+  const sequence = makeSequence(props.stage)
 
   return (<>
-  <p>{props.stage}</p>
   <p>{sequence}</p>
+
+  {/* <container>
+  <div className="button1">Button 1</div>
+  <div className="button2">Button 2</div>
+  <div className="button3">Button 3</div>
+  <div className="button4">Button 4</div>
+  </container> */}
+
+  <table className="container">
+    <div className="theMiddle"></div>
+    <tr>
+      <td className="button1">Button 1</td>
+      <td className="button2">Button 2</td>
+    </tr>
+    <tr>
+      <td className="button3">Button 3</td>
+      <td className="button4">Button 4</td>
+    </tr>
+  </table>
+
   </>)
 };
 

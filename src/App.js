8cf6d95e0c2b10c1afffd6b3f4dbebb3c0ef99e7
Ana Carolina K <anacko@gmail.com>
2021-12-07 12:42:55 -0500

    refactor challenger to pass stage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import { useState } from 'react';
+
 import './App.css';
 import Challenger from './components/Challenger';
-import { makeSequence } from './helpers/makeSequence';
 
 function App() {
-
-  const [stage, setStage] = useState(0);
-
-  const sequence = makeSequence(stage);
 
   return (
     <div className="App">
       <h1>Hello World!</h1>
-      <p>Stage: {stage}</p>
-
-      <button onClick={() => setStage(stage + 1)}>Click Me!</button>
-
-      <Challenger stage={stage} sequence={sequence}/>
+      <Challenger />
     </div>
   );
 }
--- src/components/Challenger.jsx
+++ src/components/Challenger.jsx
@@ -1,30 +1,35 @@
 import React, { useEffect, useState } from "react";
-
+import { makeSequence } from '../helpers/makeSequence';
 import './Challenger.css'
 
 const Challenger = function(props) {
 
-  let [counter, setCounter] = useState(0)
+  const [stage, setStage] = useState(3);
+  const [sequence, setSequence] = useState(makeSequence(stage));
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    setSequence(makeSequence(stage));
+  }, [stage])
 
   const handleClick = function(event) {
     const option = Number(event.target.id)
-    option === props.sequence[counter] ? console.log("yay!") : console.log("nops")
+    option === sequence[counter] ? console.log("yay!") : console.log("nops")
     setCounter(counter+1)
+
+    if (counter === stage - 1) {
+      setCounter(0)
+      setStage(stage+1)
+    }
     return option
   }
 
-  useEffect(() => {
-    const puzzleInput = function(sequence) {
-      return <div>  </div>
-    }
-  }, [])
-
   return (<>
-  <p>{props.sequence}</p>
+  {sequence? <p>{sequence}</p> : <p>No sequence defined.</p>}
 
   <table className="container">
     <tbody>
-    <tr><td className="theMiddle">{props.stage}</td></tr>
+    <tr><td className="theMiddle">{stage}</td></tr>
     <tr>
       <td className="button1" id="0" onClick={handleClick}>Button 0</td>
       <td className="button2" id="1" onClick={handleClick}>Button 1</td>

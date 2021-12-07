468a76cfeb7caf32b7fedd3a8447a2121e72c373
Ana Carolina K <anacko@gmail.com>
2021-12-06 19:25:03 -0500

    fix counter

--- src/components/Challenger.jsx
+++ src/components/Challenger.jsx
@@ -7,19 +7,23 @@ const Challenger = function(props) {
   let [counter, setCounter] = useState(0)
 
   const handleClick = function(event) {
-    setCounter(counter++)
-    //const option = Number(event.target.id)
-    //return option
+    const option = Number(event.target.id)
+    option === props.sequence[counter] ? console.log("yay!") : console.log("nops")
+    setCounter(counter+1)
+    return option
   }
 
-  useEffect((event) => {
-  },[counter])
+  useEffect(() => {
+    const puzzleInput = function(sequence) {
+      return <div>  </div>
+    }
+  }, [])
 
   return (<>
   <p>{props.sequence}</p>
 
   <table className="container">
-    <div className="theMiddle"><h1>{props.stage}</h1></div>
+    <tr><div className="theMiddle"><h1>{props.stage}</h1></div></tr>
     <tr>
       <td className="button1" id="0" onClick={handleClick}>Button 0</td>
       <td className="button2" id="1" onClick={handleClick}>Button 1</td>
@@ -29,8 +33,6 @@ const Challenger = function(props) {
       <td className="button4" id="3" onClick={handleClick}>Button 3</td>
     </tr>
   </table>
-
-  <p>{counter}</p>
 
   </>)
 };

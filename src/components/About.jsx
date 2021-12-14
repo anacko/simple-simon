a186055e58e2c8b5d53cf3e80a15c437ff5e5241
Ana Carolina K <anacko@gmail.com>
2021-12-14 18:23:10 -0500

    add about information

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,12 +10,14 @@ function About() {
   return (
     <div className="App">
       <img src={info} width="40" height="40" alt="About" onClick={toggleAbout}/>
-      <div className={`about-section ${visible}`}>
-        <p>{visible}</p>
-        <p>Press Start Game to begin</p>
+      <container className={`about-section ${visible}`}>
+        <h3>Instructions</h3>
+        <p>Press Start Game to begin. Once the sequence is presented, press the colors in the same order to go to next stage.</p>
         <p>Progress is stored locally. Press Reset Info to reset it.</p>
-        <a href="https://www.flaticon.com/packs/audio-and-video-controls" title="Source">Icons</a>
-      </div>
+        <h3>About the Game</h3>
+        <p>Simple Simon is a simple version of <a href="https://en.wikipedia.org/wiki/Simon_(game)">Simon</a> Game.</p>
+        <p>More information: <a href="https://github.com/anacko/simple-simon#readme">here</a>!</p>
+        </container>
     </div>
   );
 }

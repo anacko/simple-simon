c999387cb02a0058ca9ebe3726a0d2ee1793c744
Ana Carolina K <anacko@gmail.com>
2021-12-16 10:32:53 -0500

    remove container tag

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,14 +10,14 @@ function About() {
   return (
     <div className="App">
       <img src={info} width="40" height="40" alt="About" onClick={toggleAbout}/>
-      <container className={`about-section ${visible}`}>
+      <div className={`about-section ${visible}`}>
         <h3>Instructions</h3>
         <p>Press Start Game to begin. Once the sequence is presented, press the colors in the same order to go to next stage.</p>
         <p>Progress is stored locally. Press Reset Info to reset it.</p>
         <h3>About the Game</h3>
         <p>Simple Simon is a simple version of <a href="https://en.wikipedia.org/wiki/Simon_(game)">Simon</a> Game.</p>
         <p>More information: <a href="https://github.com/anacko/simple-simon#readme">here</a>!</p>
-        </container>
+        </div>
     </div>
   );
 }
